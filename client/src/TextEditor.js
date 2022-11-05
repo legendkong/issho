c7a807c2f980f43fadfa9418e4a24b88a6ad1acd
Jonathan Kong <jonathankong1996@gmail.com>
2022-11-06 01:13:24 +0800

    feat: persist documents

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -4,6 +4,7 @@ import 'quill/dist/quill.snow.css'
 import { io } from 'socket.io-client'
 import { useParams } from 'react-router-dom'
 
+const SAVE_INTERVAL_MS = 2000
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
@@ -41,6 +42,17 @@ export default function TextEditor() {
 
     socket.emit('get-document', documentId)
   }, [socket, quill, documentId])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const interval = setInterval(() => {
+      socket.emit('save-document', quill.getContents())
+    }, SAVE_INTERVAL_MS)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [socket, quill])
 
   useEffect(() => {
     if (socket == null || quill == null) return
