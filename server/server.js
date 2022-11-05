bb191e2ddef0baf450763337626b671195524493
Jonathan Kong <jonathankong1996@gmail.com>
2022-11-05 23:50:17 +0800

    feat: add server folder, socket.io

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const io = require('socket.io')(3001, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  socket.on('send-changes', (delta) => {
+    socket.broadcast.emit('receive-changes', delta)
+  })
+  console.log('connected')
+})
