654b4dd778f69d0e7c78a27f56ca9a500980df7e
Jonathan Kong <jonathankong1996@gmail.com>
2022-11-06 01:13:37 +0800

    feat: mongoose to store persisted data

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose')
+const Document = require('./Document')
+
+mongoose.connect('mongodb://localhost/issho')
+
 const io = require('socket.io')(3001, {
   cors: {
     origin: 'http://localhost:3000',
@@ -5,13 +10,26 @@ const io = require('socket.io')(3001, {
   }
 })
 
+const defaultValue = ''
+
 io.on('connection', (socket) => {
-  socket.on('get-document', (documentId) => {
-    const data = ''
+  socket.on('get-document', async (documentId) => {
+    const document = await findOrCreateDocument(documentId)
     socket.join(documentId) // put users in the same room
-    socket.emit('load-document', data)
+    socket.emit('load-document', document.data)
     socket.on('send-changes', (delta) => {
       socket.broadcast.to(documentId).emit('receive-changes', delta)
     })
+
+    socket.on('save-document', async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
   })
 })
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
